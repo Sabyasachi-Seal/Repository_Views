533fb9b249441a4f697a09b98dc2b8e0c7677c3d
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-01-28 12:54:02 +0530

    replolist path fix

--- repolist.py
+++ repolist.py
@@ -6,7 +6,7 @@ username = "Sabyasachi-Seal"
 request = requests.get('https://api.github.com/users/'+username+'/repos')
 jsonfile = request.json()
 
-file = open('Repository_View_Counter/config.json', 'r')
+file = open('Repository_Views/config.json', 'r')
 readfile = file.read()
 file.close()
 
@@ -20,6 +20,6 @@ for i in range(0,len(jsonfile)):
 data['repository'] = newdata
 
 jsondump = json.dumps(data)
-file = open('Repository_View_Counter/config.json', 'w')
+file = open('Repository_Views/config.json', 'w')
 file.write(jsondump)
 file.close()
